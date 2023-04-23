f101f25ba0972879e55d4d4612315e5702733636
imranhirey <imrannurhirey@gmail.com>
2023-04-23 20:11:37 +0000

    deployed

---
+++ components/Testimoni.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+
+// import react slick
+import Slider from "react-slick";
+import Image from "next/image";
+import Stars from "../public/assets/Icon/stars.svg";
+import ArrowBack from "../public/assets/Icon/eva_arrow-back-fill.svg";
+import ArrowNext from "../public/assets/Icon/eva_arrow-next-fill.svg";
+
+const Testimoni = ({
+  listTestimoni = [
+    {
+      name: "Ismail Ahmed",
+      image: "/assets/ismail.jpeg",
+      city: "mogadishu",
+      country: "Somalia",
+      rating: "4.8",
+      testimoni:
+        "I recently used Skillhob to find a skilled professional for my startup, and I have to say, it was a great experience. The platform is easy to use and I was able to connect with the right talent in just a few minutes. The support team was also very helpful and provided me with all the information I needed to make the right choice. I highly recommend Skillhob to any startup looking to find skilled professionals quickly and easily. It's a great platform!",
+    },
+    {
+      name: "Meheem Mehdi",
+      image: "/assets/people-3.png",
+      city: "Dhaka",
+      country: "Bangladesh",
+      rating: "5",
+      testimoni:
+        "I am so glad I found Skillhob! As a startup founder, finding skilled professionals can be a challenge. But Skillhob made it so easy for me to connect with the right talent. The platform is intuitive and user-friendly, and I was able to find the perfect professional for my needs in just a few minutes. The support team was also very helpful and responsive to my questions. Overall, I highly recommend Skillhob to anyone looking to find",
+    },
+  
+    {
+      name: "Nawal Mamun",
+      image: "/assets/nawal.jpg",
+      city: "Dubai",
+      country: "UAE",
+      rating: "4.5",
+      testimoni:
+        "Skillhob has been a lifesaver for my startup. As a new business owner, finding the right talent can be a daunting task. But Skillhob made it so easy for me to find and hire skilled professionals. The platform is easy to use and offers a wide range of professionals to choose from. Plus, the personalized support from the Skillhob team was outstanding. They were always available to answer any questions I had and provided me with ",
+    },
+  ],
+}) => {
+  const settings = {
+    dots: true,
+    customPaging: function (i) {
+      return (
+        <a className="">
+          <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
+        </a>
+      );
+    },
+    dotsClass: "slick-dots w-max absolute mt-20  ",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const [sliderRef, setSliderRef] = useState(null);
+
+  return (
+    <>
+      <Slider
+        {...settings}
+        arrows={false}
+        ref={setSliderRef}
+        className="flex items-stretch justify-items-stretch"
+      >
+        {listTestimoni.map((listTestimonis, index) => (
+          <div className="px-3 flex items-stretch" key={index}>
+            <div className="border-2 border-gray-500 hover:border-orange-500 transition-all rounded-lg p-8 flex flex-col">
+              <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
+                <div className="flex order-2 xl:order-1">
+                  <Image
+                    src={listTestimonis.image}
+                    height={50}
+                    width={50}
+                    alt="Icon People"
+                  />
+                  <div className="flex flex-col ml-5 text-left">
+                    <p className="text-lg text-black-600 capitalize">
+                      {listTestimonis.name}
+                    </p>
+                    <p className="text-sm text-black-500 capitalize">
+                      {listTestimonis.city},{listTestimonis.country}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
+                  <p className="text-sm">{listTestimonis.rating}</p>
+                  <span className="flex ml-4">
+                    <Stars className="h-4 w-4" />
+                  </span>
+                </div>
+              </div>
+              <p className="mt-5 text-left">“{listTestimonis.testimoni}”.</p>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      <div className="flex w-full items-center justify-end">
+        <div className="flex flex-none justify-between w-auto mt-14">
+          <div
+            className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+            onClick={sliderRef?.slickPrev}
+          >
+            <ArrowBack className="h-6 w-6 " />
+          </div>
+          <div
+            className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+            onClick={sliderRef?.slickNext}
+          >
+            <ArrowNext className="h-6 w-6" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Testimoni;
